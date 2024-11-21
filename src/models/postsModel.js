9d99bb78a913ca53b2bec96e54857e25555c5eec
Miuxa Santos <miuxasantos06@gmail.com>
2024-11-21 20:13:52 -0300

    dia 4

--- src/models/postsModel.js
+++ src/models/postsModel.js
@@ -4,11 +4,19 @@ import conectarBanco from '../config/db-config.js';
 const conexao = await conectarBanco(process.env.STRING_CONEXAO);
 
 // **Função assíncrona para buscar todos os posts do banco de dados**
-export default async function getTodosPosts(){
+export async function getTodosPosts(){
     // **Obtém o banco de dados 'imersao-dev' e a coleção 'posts'**
     const db = conexao.db("imersao-dev")
     const colecao = db.collection("posts")
   
     // **Executa a consulta e retorna os resultados como um array**
     return colecao.find().toArray();
+  }
+
+  export async function criarPost(novoPost) {
+    const db = conexao.db("imersao-dev")
+    const colecao = db.collection("posts")
+  
+    
+    return colecao.insertOne(novoPost);
   }
--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -1,5 +1,17 @@
 import express from "express";
-import { listarPosts } from "../controllers/postsControllers.js";
+import multer from "multer";
+import { listarPosts, postarNovo, uploadImagem } from "../controllers/postsControllers.js";
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+
+const executar = multer({ dest: "./uploads" , storage})
 
 const routes = (app) =>{
     // **Habilita o parser JSON para lidar com requisições com corpo em formato JSON**
@@ -7,7 +19,9 @@ const routes = (app) =>{
 
     // **Rota GET para buscar todos os posts**
     app.get("/posts", listarPosts);
-
+    // rota para criar um novo post
+    app.post("/posts", postarNovo);
+    app.post("/upload", executar.single("imagem"), uploadImagem );
 };
 
 export default routes;
